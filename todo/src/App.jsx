80016280d2fdb75e77a0bafaab99a39bdfada676
ulldecorb <ausias.bertran@gmail.com>
2021-11-26 10:38:49 +0100

    Create header component

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { TodoList } from './components/TodoList';
+import { Header } from './components/Header/Header';
+import { TodoList } from './components/TodoList/TodoList';
 import List from './constants/List';
 
 export function App () {
     return (
-        <TodoList todos={List} />
+        <>
+            <Header />
+            <TodoList todos={List} />
+        </>
     )
 }
---
+++ todo/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <header>
+            <h1>TODO LIST</h1>
+        </header>
+    )
+}
