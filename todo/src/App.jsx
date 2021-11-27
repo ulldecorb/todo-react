8aa3c9e05c8423f8d8a011d60b68dbb061a8847f
ulldecorb <ausias.bertran@gmail.com>
2021-11-27 04:37:38 +0100

    Send id checkbox to switch complete status

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useState, useRef } from 'react';
+import {v4 as newId} from 'uuid';
 import { Header } from './components/Header/Header';
 import { TodoList } from './components/TodoList/TodoList';
 import List from './constants/List';
@@ -8,23 +9,28 @@ export function App () {
 
     const todoTaskRef = useRef();
 
+    const toggleTodo = (id) => {
+        const newTodos = [...todos];
+        const todo = newTodos.find((todo) => todo.id == id);
+        todo.completed = !todo.completed;
+        setTodos(newTodos);
+    }
+
     const handleTodoAdd = () => {
         const task = todoTaskRef.current.value;
         if (task === '') return;
 
-        // const newId = todos.map((e) => e.id).sort((a, b) => a + b)[todos.length - 1] + 1;
-        const newId = todos.length;
-        const prevTodos = {id: newId, task: task, completed: false};
+        const prevTodos = {id: newId(), task: task, completed: false};
 
         setTodos([...todos,prevTodos])
 
         todoTaskRef.current.value = null;
     }
 
-    return (      
+    return (
         <Fragment>      
             <Header />
-            <TodoList todos={todos} />
+            <TodoList todos={todos} toggleTodo={toggleTodo} />
             <input ref={todoTaskRef} type="text" placeholder="New Task" />
             <button onClick={handleTodoAdd}>➕</button>
             <button>🗑</button>
